45cae32d534598b2696a7a4f7f65a4e08da47e72
inmount <inmount@163.com>
2019-08-30 17:45:09 +0800

    1.1.1908.2 使用全新解析方式处理

---
+++ TextNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dpz2.Xml {
+
+    /// <summary>
+    /// 文本节点
+    /// </summary>
+    public class TextNode : BasicNode {
+
+        /// <summary>
+        /// 获取或设置
+        /// </summary>
+        public string Value { get; set; }
+
+        /// <summary>
+        /// 对象实例化
+        /// </summary>
+        public TextNode() : base(NodeType.Text) { }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <returns></returns>
+        protected override string OnGetOuterXml() {
+            return this.Value;
+        }
+
+        /// <summary>
+        /// 获取内置文本
+        /// </summary>
+        /// <returns></returns>
+        protected override string OnGetInnerText() {
+            return this.Value;
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        protected override void OnDispose() {
+
+            this.Value = null;
+
+            base.OnDispose();
+        }
+
+    }
+}
